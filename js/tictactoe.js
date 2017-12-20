c2410b57bdaf9556972f71038d72401775ded1d6
kyotoflavor <34519888+kyotoflavor@users.noreply.github.com>
2017-12-20 16:33:30 +0100

    Add files via upload
    rien ajoute

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -1,11 +1,37 @@
 //
 
-var player1 = "X"
-var player2 = "O"
 
-var grid = [0,1,2,3,4,5,6,7,8,9]
-for (var i = 0; i < grid.length; i++) {
-  grid[i]
+var debut = 1;
+
+
+function joueur1() {
+  var joueur1 = 1;
+  if (joueur1 = 1){
+    document.getElementById("case").className += "joueur1";
+  }
+  else{
+    joueur1 = 0;
+    joueur2 = 1;
+  }
 }
 
- var cellText = document.createTextNode(grid[i]);
+function joueur2() {
+  var joueur2 = 1;
+  if (joueur2 = 1){
+  document.getElementById("case").className += "joueur2";
+  }
+  else{
+    joueur2 = 0;
+    joueur1 = 1;
+  }
+
+}
+
+function jou1jou2() {
+  if(debut=1){
+    document.getElementById("case").addEventListener( 'click', joueur1());
+  }
+  else {
+    joueur2();
+  }
+}
