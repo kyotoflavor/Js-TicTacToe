4f26e9c7f9dc7c99a3ad6a8f737c15ba81d99c35
kyotoflavor <luc.pattin@gmail.com>
2017-12-20 14:23:53 +0100

    html cree et css

---
+++ js/tictactoe.js
@@ -0,0 +1,11 @@
+//
+
+var player1 = "X"
+var player2 = "O"
+
+var grid = [0,1,2,3,4,5,6,7,8,9]
+for (var i = 0; i < grid.length; i++) {
+  grid[i]
+}
+
+ var cellText = document.createTextNode(grid[i]);
