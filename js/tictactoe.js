414038e32ace970e8c7aa919a01d86ef5ff23801
kyotoflavor <34519888+kyotoflavor@users.noreply.github.com>
2017-12-21 14:38:57 +0100

    Add files via upload
    nouveau script en test:
    selection couleur aleatoire!

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -1,37 +1,35 @@
-//
+
+    function Initialisation() {
+        joueurs = ['0', '1'];
+        //Et le joueur qui commence
+        joueur = joueurs[0];
+    }
 
 
-var debut = 1;
 
-
-function joueur1() {
-  var joueur1 = 1;
-  if (joueur1 = 1){
-    document.getElementById("case").className += "joueur1";
-  }
-  else{
-    joueur1 = 0;
-    joueur2 = 1;
-  }
-}
-
-function joueur2() {
-  var joueur2 = 1;
-  if (joueur2 = 1){
-  document.getElementById("case").className += "joueur2";
-  }
-  else{
-    joueur2 = 0;
-    joueur1 = 1;
-  }
-
-}
-
-function jou1jou2() {
-  if(debut=1){
-    document.getElementById("case").addEventListener( 'click', joueur1());
-  }
-  else {
-    joueur2();
-  }
-}
+    function jouer(zone) { //Recupère le nom de la zone cliquée via "zone"
+        //Si le joueur et egale au joueur 'X'
+        if (joueur === joueurs[0]) {
+            if (document.getElementById(zone).className.indexOf('joueur1') >= 0) {
+                joueurs[1].onclick = null;
+               } else {
+                //Sinon on definit le background de la case
+                document.getElementById(zone).className = "joueur1";
+                //On passe la main au joueur 1
+                joueur = joueurs[1];
+            }
+        }
+        else if (joueur === joueurs[1]) { //Si le joueur et egale au joueur 'O'
+            if (document.getElementById(zone).className.indexOf('joueur2') >= 0) {
+                //On affiche un alerte
+              joueurs[0].onclick = false;
+            } else {
+                //Sinon on definit le background de la case avec l'image Rond
+                document.getElementById(zone).className = "joueur2";
+                //On passe la main au joueur 2
+                joueur = joueurs[0];
+            }
+        }
+        //On fait un check :)
+      //  checking();
+    }
