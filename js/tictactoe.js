275b7c349f104137bd137d42bc81334392b51059
kyotoflavor <34519888+kyotoflavor@users.noreply.github.com>
2017-12-22 10:19:44 +0100

    Add files via upload
    nouvel version du script :
    define joueur 1 et 2.
    utiliser les class (joueur 1 et 2 du css) pour definir les couleurs de chaques joueur .

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -1,35 +1,43 @@
-
-    function Initialisation() {
-        joueurs = ['0', '1'];
-        //Et le joueur qui commence
-        joueur = joueurs[0];
-    }
+var player=1;
 
 
+/**
+ * en test
+ */
+function morpion(){
 
-    function jouer(zone) { //Recupère le nom de la zone cliquée via "zone"
-        //Si le joueur et egale au joueur 'X'
-        if (joueur === joueurs[0]) {
-            if (document.getElementById(zone).className.indexOf('joueur1') >= 0) {
-                joueurs[1].onclick = null;
-               } else {
-                //Sinon on definit le background de la case
-                document.getElementById(zone).className = "joueur1";
-                //On passe la main au joueur 1
-                joueur = joueurs[1];
-            }
-        }
-        else if (joueur === joueurs[1]) { //Si le joueur et egale au joueur 'O'
-            if (document.getElementById(zone).className.indexOf('joueur2') >= 0) {
-                //On affiche un alerte
-              joueurs[0].onclick = false;
-            } else {
-                //Sinon on definit le background de la case avec l'image Rond
-                document.getElementById(zone).className = "joueur2";
-                //On passe la main au joueur 2
-                joueur = joueurs[0];
-            }
-        }
-        //On fait un check :)
-      //  checking();
-    }
+var gridCount = [];
+var gridCell = document.getElementsByTagName('td');
+var gridRow = document.getElementsByTagName('tr');;
+var gridCounting = gridCell * gridRow;
+
+
+for (var i = 0; i < gridCell.length; i++) {
+    gridCell.push(i);
+    console.log(gridCell);
+  }
+    for (var j = 0; j < gridRow.length; j++) {
+      gridRow.push(j);
+      console.log(gridRow);
+  }
+}
+
+/**
+ * param BlocKcase
+ * juste bloque le clique du joueur si un joueur a deja rempli une case
+ */
+function joueur(BlocKcase) {
+  if (player==1){
+    BlocKcase.setAttribute("class", "joueur1");
+    player=2;
+    console.log ("joueur 1");
+  }
+  else if (player==2){
+    BlocKcase.setAttribute("class", "joueur2");
+    player=1;
+    console.log ("joueur 2");
+  }
+  BlocKcase.onclick=function(){
+    return false;
+  };
+}
