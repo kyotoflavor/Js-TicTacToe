08c4bf17a8ba2134374d9006c3d6f4902ac9c1c8
kyotoflavor <34519888+kyotoflavor@users.noreply.github.com>
2017-12-23 12:15:57 +0100

    Add files via upload
    modifier le code
    ajout de qui a jouer et qui a deja jouer la case

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -1,43 +1,38 @@
 var player=1;
 
 
-/**
- * en test
- */
-function morpion(){
-
-var gridCount = [];
-var gridCell = document.getElementsByTagName('td');
-var gridRow = document.getElementsByTagName('tr');;
-var gridCounting = gridCell * gridRow;
-
-
-for (var i = 0; i < gridCell.length; i++) {
-    gridCell.push(i);
-    console.log(gridCell);
-  }
-    for (var j = 0; j < gridRow.length; j++) {
-      gridRow.push(j);
-      console.log(gridRow);
-  }
-}
 
 /**
  * param BlocKcase
  * juste bloque le clique du joueur si un joueur a deja rempli une case
  */
 function joueur(BlocKcase) {
-  if (player==1){
-    BlocKcase.setAttribute("class", "joueur1");
-    player=2;
-    console.log ("joueur 1");
-  }
-  else if (player==2){
-    BlocKcase.setAttribute("class", "joueur2");
-    player=1;
-    console.log ("joueur 2");
-  }
-  BlocKcase.onclick=function(){
-    return false;
-  };
+    var numero = BlocKcase.id.substring(4,5);
+    if(BlocKcase.className=='joueur1' || BlocKcase.className=='joueur2'){
+        var njoueur = BlocKcase.getAttribute('jouePar'); 
+        alert('la case '+numero+'a déjà été joueur ' + njoueur);
+    } else {
+        if (player==1){
+            BlocKcase.setAttribute("class", "joueur1");
+            BlocKcase.setAttribute("jouePar",player);
+            
+            console.log ("joueur "+player+" a joue la case "+numero );
+            player=2;
+        }
+        else if (player==2){
+            BlocKcase.setAttribute("class", "joueur2");
+            BlocKcase.setAttribute("jouePar",player);
+            console.log ("joueur "+player+" a joue la case "+numero );
+            BlocKcase.setAttribute("jouePar",player);
+            player=1;
+        }
+    }
+    return;
 }
+
+/*
+if case click on retourne un element 
+    si cette element conteint la classe joueur 1 ou joueur 2 
+    alors la case est jouer 
+    sinon 
+    on peut affectuer la case */
