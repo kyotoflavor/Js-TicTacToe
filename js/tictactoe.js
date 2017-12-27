f4e736690904c98e347a3cc490a82dcc025f2dce
kyotoflavor <34519888+kyotoflavor@users.noreply.github.com>
2017-12-27 10:58:27 +0100

    Add files via upload
    final version mais sans le optionnel
    ajout de la function des combis gagnantes et des div gagnantes et draw game

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -1,6 +1,6 @@
 var player=1;
 var cpt=0;
-var nGrid=[];
+var victoire=false;
 
 
 /**
@@ -10,8 +10,6 @@ var nGrid=[];
 function joueur(morpionCase) {
     // on fait un substring pour recupere le numero de case de l'id 
     var numero = morpionCase.id.substring(4,5);
-    //debug log compte tours   
-    console.log(cpt)
     //on declare si une case a la classe name joueur 1 ou joueur 2
     if(morpionCase.className =='joueur1' || morpionCase.className =='joueur2'){
         //on lui attribu 
@@ -21,23 +19,19 @@ function joueur(morpionCase) {
     }
     else {
         if (player==1){
-            //
-            
             // applique le css de la class
             morpionCase.setAttribute("class", "joueur1");
             // applique le joueur qui a jouer la case 
             morpionCase.setAttribute("jouePar",player);
-            // debug log
-            console.log ("joueur "+player+" a joue la case "+numero );
             //on affiche le resultat des tours des joueurs
             document.getElementById('histo1').innerHTML += "<br>joueur "+player+" a joue la case "+numero+" au tour n° "+cpt;
-            // push les valeur
-            nGrid.push(player+numero);
-            // debug log
-            console.log(nGrid);
-            // on increment le compteur a chaque click
+             // on increment le compteur a chaque click
             cpt++;
-            // on passe la main au second joueur 
+            if (victoire == true) {
+            disp_win();
+            //alert("La partie est terminée! Le joueur "+joueur.numero+" a gagné!");
+            } 
+           // on passe la main au second joueur 
             player=2;
         }
         
@@ -46,75 +40,98 @@ function joueur(morpionCase) {
             morpionCase.setAttribute("class", "joueur2");
              // applique le joueur qui a jouer la case 
             morpionCase.setAttribute("jouePar",player);
-            // debug log
-            console.log ("joueur "+player+" a joue la case "+numero );
             //on affiche le resultat des tours des joueurs
             document.getElementById('histo2').innerHTML += "<br>joueur "+player+" a joue la case "+numero+" au tour n° "+cpt;
-            // push les valeur
-            nGrid.push(player+numero);
-            // debug log
-            console.log(nGrid);
             // on increment le compteur a chaque click
             cpt++;
             // on passe la main au premier joueur 
             player=1;
         }
-        if(cpt>=9){
-            //
-            cpt=9;
-            player==0;
-            alert('end game');
-        }
     }
-    return checkWin();
+    if(cpt>8){
+        disp_draw();
+        }
+    return checkWin(); 
 }
-
 
 function checkWin(){
         //ligne horizontal
-        if(case0.className === "joueur1" && case1.className === "joueur1" && case2.className === "joueur1"){
+        if(case0.className === "joueur1" && case1.className === "joueur1" && case2.className === "joueur1"){  disp_win_p1();
   }
-  else if (case3.className === "joueur1" && case4.className === "joueur1" && case5.className === "joueur1"){
+  else if (case3.className === "joueur1" && case4.className === "joueur1" && case5.className === "joueur1"){  disp_win_p1();
   }
-  else if (case6.className === "joueur1" && case7.className === "joueur1" && case8.className === "joueur1"){
+  else if (case6.className === "joueur1" && case7.className === "joueur1" && case8.className === "joueur1"){  disp_win_p1();
    }
 
 
     //ligne vertical
-    else if (case0.className === "joueur1" && case3.className === "joueur1" && case6.className === "joueur1"){
+    else if (case0.className === "joueur1" && case3.className === "joueur1" && case6.className === "joueur1"){  disp_win_p1();
    }
-    else if (case1.className === "joueur1" && case4.className === "joueur1" && case7.className === "joueur1"){
+    else if (case1.className === "joueur1" && case4.className === "joueur1" && case7.className === "joueur1"){  disp_win_p1();
    }
-    else if (case2.className === "joueur1" && case5.className === "joueur1" && case8.className === "joueur1"){
+    else if (case2.className === "joueur1" && case5.className === "joueur1" && case8.className === "joueur1"){  disp_win_p1();
    }
 
    // diagonal
-    else if (case0.className === "joueur1" && case4.className === "joueur1" && case8.className === "joueur1"){
+    else if (case0.className === "joueur1" && case4.className === "joueur1" && case8.className === "joueur1"){  disp_win_p1();
    }
-    else if (case2.className === "joueur1" && case4.className === "joueur1" && case6.className === "joueur1"){
+    else if (case2.className === "joueur1" && case4.className === "joueur1" && case6.className === "joueur1"){  disp_win_p1();
    }
+
 
         //ligne horizontal
-        if(case0.className === "joueur2" && case1.className === "joueur2" && case2.className === "joueur2"){
+        if(case0.className === "joueur2" && case1.className === "joueur2" && case2.className === "joueur2"){  disp_win_p2();
+     disp_win();
    }
-  else if (case3.className === "joueur2" && case4.className === "joueur2" && case5.className === "joueur2"){
+  else if (case3.className === "joueur2" && case4.className === "joueur2" && case5.className === "joueur2"){  disp_win_p2();
    }
-  else if (case6.className === "joueur2" && case7.className === "joueur2" && case8.className === "joueur2"){
+  else if (case6.className === "joueur2" && case7.className === "joueur2" && case8.className === "joueur2"){  disp_win_p2();
    }
 
 
     //ligne vertical
-    else if (case0.className === "joueur2" && case3.className === "joueur2" && case6.className === "joueur2"){
+    else if (case0.className === "joueur2" && case3.className === "joueur2" && case6.className === "joueur2"){  disp_win_p2();
    }
-    else if (case1.className === "joueur2" && case4.className === "joueur2" && case7.className === "joueur2"){
+    else if (case1.className === "joueur2" && case4.className === "joueur2" && case7.className === "joueur2"){  disp_win_p2();
    }
-    else if (case2.className === "joueur2" && case5.className === "joueur2" && case8.className === "joueur2"){
+    else if (case2.className === "joueur2" && case5.className === "joueur2" && case8.className === "joueur2"){  disp_win_p2();
    }
 
    // diagonal
-    else if (case0.className === "joueur2" && case4.className === "joueur2" && case8.className === "joueur2"){
+    else if (case0.className === "joueur2" && case4.className === "joueur2" && case8.className === "joueur2"){  disp_win_p2();
    }
-    else if (case2.className === "joueur2" && case4.className === "joueur2" && case6.className === "joueur2"){
+    else if (case2.className === "joueur2" && case4.className === "joueur2" && case6.className === "joueur2"){  disp_win_p2();
    }
-return false;
+
+   return false;
+}
+
+function disp_win_p1() {
+    if (victoire=true){
+        document.getElementById("win_div").style.visibility = "visible";
+        document.getElementById("win_div").innerHTML = '<br>Le joueur 1 a gagné! <br><img style="height: auto; width: auto;" src="css/i-win-you-lose.png"/> <br> <button onclick="fermer()">FERMER</button>';
+    }   
+  }
+
+  function disp_win_p2() {
+    if (victoire=true){
+        document.getElementById("win_div").style.visibility = "visible";
+        document.getElementById("win_div").innerHTML = '<br>Le joueur 1 a gagné! <br><img style="height: auto; width: auto;" src="css/i-win-you-lose.png"/> <br> <button onclick="fermer()">FERMER</button>';
+    }   
+  }
+
+ function disp_draw() {
+    if (draw=true){
+        document.getElementById("win_div").style.visibility = "visible";
+        document.getElementById("win_div").innerHTML = '<br>draw game! <br><img style="height: auto; width: auto;" src="css/draw.png"/> <br> <button onclick="fermer()">FERMER</button>';
+    }   
+  }
+
+  /*Fonction du bouton fermer */
+function fermer() {
+  if (document.getElementById("win_div").style.visibility = "visible") {
+      document.getElementById("win_div").style.visibility = "hidden";
+      window.location.reload(true);
+  }
+
 }
